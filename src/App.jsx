d616d2508224d17947af1845128d8762a08faeae
kartick17 <karticksadhu0@gmail.com>
2024-07-22 23:25:45 +0530

    Fetch order

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import Home from './ui/Home'
 import Cart from './features/cart/Cart'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import CreateOrder from './features/order/CreateOrder'
-import Order from './features/order/Order'
+import Order, { loader as orderLoader } from './features/order/Order'
 import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
-      { path: '/', element: <Home /> },
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        element: <Order />,
+        loader: orderLoader,
+        path: '/order/:orderId',
+        errorElement: <Error />,
+      },
       { path: '/cart', element: <Cart /> },
-      { path: '/menu', element: <Menu />, loader: menuLoader },
       { path: '/order/new', element: <CreateOrder /> },
-      { path: '/order/:orderId', element: <Order /> },
     ],
   },
 ])
