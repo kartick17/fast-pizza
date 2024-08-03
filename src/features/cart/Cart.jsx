22d42e70a48e124b946f79d6707553a457524826
kartick17 <karticksadhu0@gmail.com>
2024-08-03 20:34:58 +0530

    Degigned cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import LinkButton from '../../ui/LinkButton'
 import Button from '../../ui/Button'
+import CartItem from './CartItem'
 
 const fakeCart = [
   {
@@ -29,16 +30,21 @@ function Cart() {
   const cart = fakeCart
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <LinkButton to='/menu'>&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className='mt-8 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <ul className='mt-3 divide-y-2 divide-stone-200 border-b-2'>
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
 
-      <div>
+      <div className='mt-8 space-x-3'>
         <Button to='/order/new' type='primary'>
           Order pizzas
         </Button>
-        <Button type='primary'>Clear cart</Button>
+        <Button type='secondary'>Clear cart</Button>
       </div>
     </div>
   )
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ' px-4 py-3 sm:px-6 sm:py-3',
     small: base + ' px-4 py-2 sm:px-5 sm:py-2.5 text-xs',
+    secondary:
+      'inline-block disabled:cursor-not-allowed rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring border-2 border-stone-200 focus:ring-stone-200 focus:ring-offset-2 px-4 py-2.5 sm:px-6 sm:py-3',
   }
 
   if (to)
