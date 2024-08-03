5a67b4713548b20758b5dedbc314ee74b5087168
kartick17 <karticksadhu0@gmail.com>
2024-08-03 21:11:55 +0530

    Degigned order form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom'
 
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block disabled:cursor-not-allowed rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2'
+    'inline-block text-sm disabled:cursor-not-allowed rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2'
 
   const styles = {
     primary: base + ' px-4 py-3 sm:px-6 sm:py-3',
     small: base + ' px-4 py-2 sm:px-5 sm:py-2.5 text-xs',
     secondary:
-      'inline-block disabled:cursor-not-allowed rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring border-2 border-stone-200 focus:ring-stone-200 focus:ring-offset-2 px-4 py-2.5 sm:px-6 sm:py-3',
+      'inline-block text-sm disabled:cursor-not-allowed rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring border-2 border-stone-200 focus:ring-stone-200 focus:ring-offset-2 px-4 py-2.5 sm:px-6 sm:py-3',
   }
 
   if (to)
